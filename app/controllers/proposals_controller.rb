55b5dc1fcd6cd74f7997a4b027b73b6fd57afbfb
Natalie Ruiz <email": null>
2017-05-31 15:10:26 -0400

    Create route skeleton

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,3 +1,31 @@
 class ProposalsController < ApplicationController
 
+
+  def index
+      # @proposals = Proposal.all
+    end
+
+    def show
+      # @proposal = Proposal.find(proposal_params)
+    end
+
+    def new
+      # @proposal = Proposal.new
+    end
+
+    def create
+
+    end
+
+    def edit
+    end
+
+    def update
+
+    end
+
+    private
+      def proposal_params
+         params.require(:proposal).permit(:summary, :hypothesis, :status, :title)
+      end
 end
