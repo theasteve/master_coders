d6e21764dbd2012c0577bb08aaf146baeea3ae2b
Natalie Ruiz <email": null>
2017-05-31 15:17:52 -0400

    start creating link for each proposal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :proposals, :except[:delete]
+  root :to => "proposals#index"
+
+  resources :proposals, except: [:delete]
+
+
 end
