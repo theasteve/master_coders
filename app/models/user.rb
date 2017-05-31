bc14e7909f6fe609bb3ed6635f1dc910c295e39e
Richard Kim <email": null>
2017-05-31 15:45:48 -0400

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :proposals, class_name: :proposals, foreign_key: :requestor_id
-  has_many :experiments, classname: :proposals, foreign_key: :experimentor_id
+  has_many :experiments, class_name: :proposals, foreign_key: :experimentor_id
   has_secure_password
 end
