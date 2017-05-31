934c32df874c828975c3589f0997829e0f30a273
Natalie Ruiz <natnatruiz@gmail.com>
2017-05-31 16:16:02 -0400

    Update 20170531191105_create_proposals.rb

--- db/migrate/20170531191105_create_proposals.rb
+++ db/migrate/20170531191105_create_proposals.rb
@@ -1,10 +1,10 @@
 class CreateProposals < ActiveRecord::Migration[5.0]
   def change
     create_table :proposals do |t|
-      t.string :title 
-      t.text :summary
-      t.string :hypothesis
-      t.string :status
+      t.string :title, null: false
+      t.text :summary, null: false
+      t.string :hypothesis, null: false
+      t.string :status, null: false
       t.references :requestor
       t.references :experimentor
 
