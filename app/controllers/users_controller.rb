81750e3039b70561f6ff5a64846a2031d95c31b3
Natalie Ruiz <natnatruiz@gmail.com>
2017-05-31 16:10:15 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :password_digest, :type)
+      params.require(:user).permit(:username, :password, :type)
     end
 
 end
