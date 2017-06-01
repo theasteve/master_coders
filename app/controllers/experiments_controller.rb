4e0495edb41fa46fb57d44d880191eefbbbeaf88
Lou Gaeta <email": null>
2017-06-01 12:20:01 -0400

    Adds route protection for new,edit,create, update for experiments and proposals

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -8,26 +8,36 @@ class ExperimentsController < ApplicationController
 
   def new
     @experiment = Experiment.new
+    redirect_to proposal_path(@proposal) unless logged_in? && !@proposal.experiment
   end
 
   def edit
+    redirect_to proposal_experiment_path(@experiment.proposal, @experiment) unless experimentor?(@proposal)
   end
 
   def create
     @experiment = Experiment.new(experiment_params)
-    @proposal.assign_attributes(experimentor_id: current_user.id)
-    @experiment.proposal = @proposal
-    if @experiment.save
-      @proposal.status = "in progress"
-      @proposal.save
-      redirect_to @proposal
+    if logged_in? && !@proposal.experiment
+      @proposal.assign_attributes(experimentor_id: current_user.id)
+      @experiment.proposal = @proposal
+      if @experiment.save
+        @proposal.status = "in progress"
+        @proposal.save
+        redirect_to @proposal
+      else
+        render 'new'
+      end
     else
-      render 'new'
+      redirect_to proposal_path(@proposal)
     end
   end
 
   def update
-    if @experiment.update(experiment_params)
+    if experimentor?(@proposal)
+      if @experiment.update(experiment_params)
+        redirect_to proposal_experiment_path(@experiment.proposal, @experiment)
+      end
+    else
       redirect_to proposal_experiment_path(@experiment.proposal, @experiment)
     end
   end
