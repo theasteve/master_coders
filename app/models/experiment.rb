81c35ff0d54a12490986030b5d10ec007263f6d8
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-01 11:35:12 -0400

    Create observation model for polymorophic between experiment and procedure

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,6 +1,6 @@
 class Experiment < ApplicationRecord
   belongs_to :proposal
   # has_many :procedures
-  # has_many :observations, as :observable
+  has_many :observations, as :observable
   validates :name, :description, presence: true
 end
--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,2 +1,3 @@
 class Observation < ApplicationRecord
+  belongs_to :observable, polymorphic: true
 end
