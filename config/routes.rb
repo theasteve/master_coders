d61934f8c83e10c9bddc4edc6390d965dffd2483
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-01 14:45:56 -0400

    create routes for a new Observation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  get 'observations/new'
+
+  get 'observations/create'
+
+  get 'observations/edit'
+
+  get 'observations/update'
+
+  get 'observations/destroy'
+
+  get 'observations/show'
+
+
   root "proposals#index"
 
   resources :proposals, except: :delete do
     resources :experiments, except: [:delete, :index] do
+    end
   end
 
   resources :users, only: [:new, :create]
