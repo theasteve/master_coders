7aa0363fd2c1d6614e1ef2e7eb6adc480118b1e8
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-06-01 15:18:17 -0400

    refactor roustes controller nesting observations into experiments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,14 @@
 Rails.application.routes.draw do
-  get 'observations/new'
-
-  get 'observations/create'
-
-  get 'observations/edit'
-
-  get 'observations/update'
-
-  get 'observations/destroy'
-
-  get 'observations/show'
 
 
   root "proposals#index"
 
   resources :proposals, except: :delete do
-<<<<<<< HEAD
+
     resources :experiments, except: [:delete, :index] do
+      resources :observations, only: [:new, :create, :show] 
     end
-=======
-    resources :experiments, except: [:delete, :index]
->>>>>>> d0a8a09ff92c5e911b047fef7ffbd0e9797b319b
-  end
+
 
   resources :users, only: [:new, :create]
 
