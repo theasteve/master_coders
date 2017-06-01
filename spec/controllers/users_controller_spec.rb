209d0ef9431c97105a0dbb835c579299c8f488bc
Lana Chiad <lanachiad7@gmail.com>
2017-06-01 11:33:08 -0400

    Create sample faculty and staff user for testing user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  
+  # Faculty user
+  let!(:faculty) { User.create!(username: "lana2", password_digest: "1234", role: "Faculty") }
+  # Staff user
+  let!(:labtech) { User.create!(username: "lana3", password_digest: "1234", role: "Staff") }
 
 end
